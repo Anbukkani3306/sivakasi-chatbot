df216c54fd91f58c8a5e0fd75c66c399590eba00
anbu <lovefruit3306@gmail.com>
2025-06-12 13:25:34 +0530

    Update app.py and deploy settings

--- app.py
+++ app.py
@@ -60,8 +60,8 @@ def chat():
     reply = generate_gemini_response(user_message, pdf_chunks)
     return jsonify({"response": reply})
 
-#@if __name__ == "__main__":#
-#    app.run(debug=True)#
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+#@if __name__ == "__main__":
+#    app.run(debug=True)
+#if __name__ == "__main__":
+  #  app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
